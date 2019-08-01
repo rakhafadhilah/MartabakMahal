04298707eab150dea6442bb1beecb9961436d90c
Rizky I. F <rykzius@gmail.com>
2019-08-01 19:47:01 +0700

    Jadi sedikit lumayan jalan sekarang.

--- MartabakMahal/server.js
+++ MartabakMahal/server.js
@@ -1,16 +1,20 @@
 'use strict';
 const express = require('express');
+const bp = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
 app.post('/hehe', function (req, res) {
-    res.send(req.params);
+    res.send(req.body);
+    console.log(req.body);
 });
 
 app.listen(port, () => console.log(`Aplikasi Martabak Mahal listening on port ${port}!`));
