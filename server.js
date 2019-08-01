01c6e71dc38bea23bd107ac97a02f9a5ce581411
Rizky I. F <rykzius@gmail.com>
2019-08-01 23:55:40 +0700

    Perbaikan dan uji coba awal sistem login (user: bob, password: bob, logout = salahin user / password)

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 'use strict';
 const express = require('express');
-const bp = require('body-parser');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 80;
 
 app.use(express.static('public'));
-app.use(express.urlencoded()); //parse url-encoded body
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true })); //parse url-encoded body
 app.use(express.json()); //parse .json body (hati2 lupa ini)
 
+// halaman utama
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    if (req.cookies.isLoggedIn === 'yes') {
+        res.redirect('/pagekedua.html');
+    } else {
+        res.redirect('/login.html');
+    }
 });
 
+// penerima request post untuk login
 app.post('/post_login', function (req, res) {
-    res.send(req.body);
-    console.log(req.body);
+    if (req.body.username === 'bob' && req.body.password === 'bob') {
+        res.cookie('isLoggedIn', 'yes');
+    } else {
+        res.cookie('isLoggedIn', 'no');
+    }
+    res.redirect('../');
 });
 
-app.listen(port, () => console.log(`Aplikasi Martabak Mahal listening on port ${port}!`));
+app.listen(port, () => console.log(`Aplikasi POS 'Martabak Mahal' telah dijalankan pada port ${port}!`));
