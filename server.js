71daa5a6c43bb7462164f6a7ff1caf355e4d3762
Rizky I. F <rykzius@gmail.com>
2019-08-05 22:58:20 +0700

    Ganti semua .html jadi .ejs supaya tiap laman menjadi lebih dinamis dan dapat memanfaatkan EJS View Engine

--- server.js
+++ server.js
@@ -2,20 +2,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const ejs = require('ejs');
 const app = express();
-const port = 80;
+const port = 3000;
 
-app.use(express.static('public'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); //parse url-encoded body
+app.use(express.static('public'));
 app.use(express.json()); //parse .json body (hati2 lupa ini)
+
+app.set('view engine', 'ejs');
+
+app.get('/login', function (req, res) {
+    res.render('login', {});
+});
+
+app.get('/pagepertama', function (req, res) {
+    res.render('pagepertama', {});
+});
+
+app.get('/pagekedua', function (req, res) {
+    res.render('pagekedua', {});
+});
+
+app.get('/pageketiga', function (req, res) {
+    res.render('pageketiga', {});
+});
+
+app.get('/test', function (req, res) {
+    res.render('test', {});
+});
 
 // halaman utama
 app.get('/', function (req, res) {
     if (req.cookies.isLoggedIn === 'yes') {
-        res.redirect('/pagekedua.html');
+        res.redirect('/pagekedua');
     } else {
-        res.redirect('/login.html');
+        res.redirect('/login');
     }
 });
 
